788fd819df9a3493f8f2cce7a9f763daef6b269a
sergenp <sergen12343@gmail.com>
2020-02-28 11:40:55 +0300

    added a main method

--- src/PlayerMainForm.java
+++ src/PlayerMainForm.java
@@ -6,4 +6,12 @@ public class PlayerMainForm {
     private JButton pauseButton;
     private JButton nextButton;
     private JList list1;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("PlayerMainForm");
+        frame.setContentPane(new PlayerMainForm().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
