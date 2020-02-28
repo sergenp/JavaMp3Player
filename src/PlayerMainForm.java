2b003fe301830aac635cd3e44107793394be89ab
sergenp <sergen12343@gmail.com>
2020-02-28 11:25:00 +0300

    Initial commit

---
+++ src/PlayerMainForm.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class PlayerMainForm {
+    private JPanel panel1;
+    private JButton playButton;
+    private JButton pauseButton;
+    private JButton nextButton;
+    private JList list1;
+}
